777e813821edd0f2e3c4b2a35d9bf0e672b554f7
plohm12 <plohm12@gmail.com>
2014-10-15 22:55:57 -0400

    Updating Seisimic Activity Map

--- SesimicActivityMap/src/com/cs307/sesimicactivitymap/SesimicActicityMap.java
+++ SesimicActivityMap/src/com/cs307/sesimicactivitymap/SesimicActicityMap.java
@@ -1,9 +1,12 @@
 package com.cs307.sesimicactivitymap;
 
+import java.util.ArrayList;
+
 import com.vaadin.tapio.googlemaps.GoogleMap;
 import com.vaadin.tapio.googlemaps.client.LatLon;
 import com.vaadin.tapio.googlemaps.client.overlays.GoogleMapInfoWindow;
 import com.vaadin.tapio.googlemaps.client.overlays.GoogleMapMarker;
+import com.vaadin.tapio.googlemaps.client.overlays.GoogleMapPolygon;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.VerticalLayout;
@@ -39,6 +42,39 @@ public class SesimicActicityMap {
 		layout.setMargin(true);
 		layout.setSizeFull();
 		layout.setHeightUndefined();
+		
+		//A few fake map zones in order to demonstrate color filling
+				//
+				//San Francisco area
+				ArrayList<LatLon> sfcoords = new ArrayList<LatLon>();
+				sfcoords.add(new LatLon(36.4, -121.6));
+				sfcoords.add(new LatLon(37.1, -122.3));
+				sfcoords.add(new LatLon(38.4, -123.0));
+				sfcoords.add(new LatLon(37.8, -120.0));
+				sfcoords.add(new LatLon(36.7, -119.8));
+				sfcoords.add(new LatLon(36.4, -121.6));
+				
+				GoogleMapPolygon sanfran = new GoogleMapPolygon(sfcoords);
+				sanfran.setFillColor("#FF0000");
+				sanfran.setFillOpacity(.35);
+				sanfran.setStrokeColor("#FF0000");
+				sanfran.setStrokeOpacity(.8);
+				googleMap.addPolygonOverlay(sanfran);
+				
+				//Midwest area
+				ArrayList<LatLon> mwcoords = new ArrayList<LatLon>();
+				mwcoords.add(new LatLon(43, -103));
+				mwcoords.add(new LatLon(41, -93));
+				mwcoords.add(new LatLon(36, -91));
+				mwcoords.add(new LatLon(37, -102));
+				mwcoords.add(new LatLon(43, -103));
+				
+				GoogleMapPolygon midwest = new GoogleMapPolygon(mwcoords);
+				midwest.setFillColor("#00FF00");
+				midwest.setFillOpacity(.35);
+				midwest.setStrokeColor("#00FF00");
+				midwest.setStrokeOpacity(.8);
+				googleMap.addPolygonOverlay(midwest);
 	}
 	public VerticalLayout getLayout() {
 		return layout;
